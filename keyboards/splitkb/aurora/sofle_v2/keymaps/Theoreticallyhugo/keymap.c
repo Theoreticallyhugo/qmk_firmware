58fa11ca558def8d109b1c7732a76ed16c00ada1
Theoreticallyhugo <hugo@raballo.de>
2023-11-08 00:41:14 +0100

    added enter and backspace on thumbkeys for when only one hand is free

--- keyboards/splitkb/aurora/sofle_v2/keymaps/Theoreticallyhugo/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/Theoreticallyhugo/keymap.c
@@ -15,11 +15,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // THUMBS are supposed to use exactly two keys each. homing are the layer keys and next to them is lctl and spc
     // backspace and enter are on layer two, where enter replaces space on the thumb, and backspace is under the pinky
     // however, i do need command (LGUI) a lot for macos, so its close by
+    // THE THUMB ENTER AND BACKSPACE ARE ONLY PROVIDED FOR WHEN THEY ARE NEEDED OUTSIDE OF TEN FINGER TYPING
+    // LIKE WHEN ONLY ONE HAND IS FREE
 	[0] = LAYOUT(KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
                  KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC,
                  KC_ESC , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
                  KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_MUTE, _______, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                                   KC_LOPT, _______, KC_LCTL, MO(2)  , KC_LGUI, KC_LGUI, KC_SPC , MO(1)  , _______, KC_ROPT),
+                                   KC_LOPT, KC_ENT , KC_LCTL, MO(2)  , KC_LGUI, KC_LGUI, KC_SPC , MO(1)  , KC_BSPC, KC_ROPT),
 
     // SPECIAL CHARACTERS
     // on this layer you get the function keys in the top row
