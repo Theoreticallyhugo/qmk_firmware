8cf68bd87b60ff2ffa5d0fb3078c145dd12e5f5b
Theoreticallyhugo <me@theoreticallyhugo.com>
2025-05-21 15:04:13 +0200

    improved gaming layer

--- keyboards/splitkb/aurora/sofle_v2/keymaps/th/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/th/keymap.c
@@ -37,8 +37,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[1] = LAYOUT(_______   , KC_F1     , KC_F2  , KC_F3    , KC_F4    , KC_F5  ,                   KC_F6   , KC_F7  , KC_F8  , KC_F9  , KC_F10    , KC_F11 ,
                  KC_RBRC   , KC_1      , KC_2   , KC_3     , KC_4     , KC_5   ,                   KC_6    , KC_7   , KC_8   , KC_9   , KC_0      , KC_MINS,
                  S(KC_RBRC), S(KC_1)   , S(KC_2), S(KC_3)  , S(KC_4)  , S(KC_5),                   S(KC_6) , S(KC_7), S(KC_8), S(KC_9), S(KC_0)   , S(KC_MINS),
-                 KC_NUBS   , S(KC_NUBS), KC_EQL , S(KC_EQL), LSA(KC_7), A(KC_5), KC_MPLY, XXXXXXX, A(KC_6) , A(KC_7), A(KC_8), A(KC_9), S(KC_NUHS), KC_NUHS,
-                                         _______,   _______, KC_LOPT  , MO(3)  , _______, XXXXXXX, _______ , XXXXXXX, XXXXXXX, XXXXXXX),
+                 KC_NUBS   , S(KC_NUBS), KC_EQL , S(KC_EQL), RSA(KC_7), RALT(KC_5), KC_MPLY, XXXXXXX, RALT(KC_6) , RALT(KC_7), RALT(KC_8), RALT(KC_9), S(KC_NUHS), KC_NUHS,
+                                         _______,   _______, KC_RALT  , MO(3)  , _______, XXXXXXX, _______ , XXXXXXX, XXXXXXX, XXXXXXX),
 
     // NAVIGATION
     // this layer is for NAVIGATION
@@ -54,18 +54,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // SYSTEM CONTROL
     // control music, volume, sleep and the mouse
     // first row switch unicode input to the respective OS
-	[3] = LAYOUT(_______, _______, UC_MAC , UC_LINX, UC_WIN , _______,                   _______, _______, _______, _______, _______, _______,
-	             _______, _______, _______, KC_SLEP, KC_WAKE, _______,                   KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_ACL0, _______,
-	             _______, _______, KC_VOLD, KC_MUTE, KC_VOLU, _______,                   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_ACL1, KC_ACL2,
-	             _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, XXXXXXX, KC_PWR , _______, KC_BTN1, KC_BTN2, _______, _______, _______,
+	[3] = LAYOUT(DF(4)  , _______, _______, _______, _______, KC_SLEP,                   _______, _______, _______, _______, _______, _______,
+	             _______, _______, _______, _______, _______, _______,                   KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_ACL0, _______,
+	             KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_MUTE, KC_VOLU,                   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_ACL1, KC_ACL2,
+	             _______, _______, _______, _______, _______, _______, XXXXXXX, KC_PWR , _______, KC_BTN1, KC_BTN2, _______, _______, _______,
                                    XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX),
 
     // GAYMING
-	[4] = LAYOUT(KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
+	[4] = LAYOUT(KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
                  KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC,
-                 KC_ESC , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+                 KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
                  KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_MUTE, _______, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                                   KC_LOPT, KC_ENT , KC_LCTL, KC_SPC , KC_LGUI, KC_LOPT, KC_SPC , KC_ROPT, KC_BSPC, DF(0) ),
+                                   KC_LOPT, KC_ENT , KC_LALT, KC_SPC , KC_LGUI, KC_RALT, KC_SPC , MO(1)  , KC_BSPC, DF(0) ),
 };
 
 
