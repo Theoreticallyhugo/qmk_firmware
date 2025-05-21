6a761ca4dbab312033ff736e7b672a7500c908c3
Theoreticallyhugo <me@theoreticallyhugo.com>
2025-05-21 14:21:32 +0200

    awesome linux adjustments

---
+++ compile_sofle.sh
@@ -0,0 +1,2 @@
+qmk compile -kb splitkb/aurora/sofle_v2 -km th -e CONVERT_TO=elite_pi -j 4
+# connect elite_pi whilst holding top "inner" button down. then mount
--- keyboards/splitkb/aurora/sofle_v2/keymaps/th/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/th/keymap.c
@@ -20,11 +20,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // however, i do need command (LGUI) a lot for macos, so its close by
     // THE THUMB ENTER AND BACKSPACE ARE ONLY PROVIDED FOR WHEN THEY ARE NEEDED OUTSIDE OF TEN FINGER TYPING
     // LIKE WHEN ONLY ONE HAND IS FREE
-	[0] = LAYOUT(KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
-                 KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC,
-                 MT(MOD_LCTL, KC_ESC) , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-                 OSM(MOD_LSFT), KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_MUTE, _______, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, OSM(MOD_RSFT),
-                                   KC_LCTL, KC_ENT , KC_ROPT, MO(2)  , KC_LGUI, OSM(MOD_RALT), KC_SPC , MO(1)  , KC_BSPC, KC_ROPT),
+	[0] = LAYOUT(KC_GRV               , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                         KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
+                 KC_TAB               , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                         KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC,
+                 MT(MOD_LCTL, KC_ESC) , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                         KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+                 OSM(MOD_LSFT)        , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_MUTE,       _______, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, OSM(MOD_RSFT),
+                                                 KC_LCTL, KC_ENT , KC_LALT, MO(2)  , KC_LGUI, OSM(MOD_RALT), KC_SPC , MO(1)  , KC_BSPC, KC_ROPT),
 
     // SPECIAL CHARACTERS
     // on this layer you get the function keys in the top row
@@ -34,11 +34,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // <, >, ´, `, backslash, [,        ], |, {, }, ', #
     // the fifth row provides one with either another layer or the option key, which is only needed for exotic symbols, as all the others are easily accessible
     // the grave key for the symbols ^ and ° is so rarely used, that its replaced on this layer, and only accessible through the first layer
-	[1] = LAYOUT(_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                          KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-                 KC_RBRC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                          KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
-                 S(KC_RBRC), S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),                        S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), S(KC_MINS),
-                 KC_NUBS, S(KC_NUBS), KC_EQL , S(KC_EQL), LSA(KC_7), A(KC_5), KC_MPLY, XXXXXXX, A(KC_6) ,A(KC_7), A(KC_8), A(KC_9), S(KC_NUHS), KC_NUHS,
-                                   _______, _______, KC_LOPT, MO(3)  , _______,        XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX),
+	[1] = LAYOUT(_______   , KC_F1     , KC_F2  , KC_F3    , KC_F4    , KC_F5  ,                   KC_F6   , KC_F7  , KC_F8  , KC_F9  , KC_F10    , KC_F11 ,
+                 KC_RBRC   , KC_1      , KC_2   , KC_3     , KC_4     , KC_5   ,                   KC_6    , KC_7   , KC_8   , KC_9   , KC_0      , KC_MINS,
+                 S(KC_RBRC), S(KC_1)   , S(KC_2), S(KC_3)  , S(KC_4)  , S(KC_5),                   S(KC_6) , S(KC_7), S(KC_8), S(KC_9), S(KC_0)   , S(KC_MINS),
+                 KC_NUBS   , S(KC_NUBS), KC_EQL , S(KC_EQL), LSA(KC_7), A(KC_5), KC_MPLY, XXXXXXX, A(KC_6) , A(KC_7), A(KC_8), A(KC_9), S(KC_NUHS), KC_NUHS,
+                                         _______,   _______, KC_LOPT  , MO(3)  , _______, XXXXXXX, _______ , XXXXXXX, XXXXXXX, XXXXXXX),
 
     // NAVIGATION
     // this layer is for NAVIGATION
